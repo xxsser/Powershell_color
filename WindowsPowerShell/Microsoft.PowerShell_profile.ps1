af66e4bddad6da6dae3e16ed3791f9985a0d717a
wenkai <wenkai@soi.so>
2016-08-15 22:11:53 +0800

    update

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,8 @@ Import-Module PsColor
 
 function Prompt
 {
+    Write-Host ('$ ') -NoNewline -ForegroundColor 'white'
     Write-Host ($(Get-Location)) -NoNewline -ForegroundColor 'cyan'
+    Write-Host ('>') -NoNewline -ForegroundColor 'gray'
     return ' '
 }
